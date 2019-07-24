1794a2145942fc31b4ff1c515c0ddbd69a00a2d1
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-07-24 17:23:57 +0300

    Update task.js

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,26 @@
+const dropdownValue = document.querySelector('.dropdown__value');
+const dropdownList = document.querySelector('.dropdown__list');
+const dropdownItem = [...document.querySelectorAll('.dropdown__item')],
+const dropdownItemLength = dropDownItem.length;
+
+function showDropdown() {
+    if (!dropdownList.classList.contains('dropdown__list_active')) {
+        dropdownList.classList.add('dropdown__list_active');
+    } else {
+        dropdownList.classList.remove('dropdown__list_active');
+    };
+};
+
+dropdownValue.addEventListener('click', () => {
+    showDropdown();
+});
+
+
+
+for ( var i = 0; i < dropdownItemLength; i++) {
+    dropdownItem[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        dropdownList.classList.remove('dropdown__list_active');
+        dropdownValue.textContent = dropdownItem[i].textContent;
+    });
+};
