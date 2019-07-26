475c9487bec60b0631da084bdedfa96c130bcd37
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-07-26 15:05:22 +0300

    Update task.js

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,33 +1,25 @@
 const listOfInterests = document.querySelector('.interests_main');
 
 listOfInterests.addEventListener('change', function (e) {
-        
         if (e.target.closest('.interest').querySelector('.interests')) {
-     
-            const arr = [...e.target.closest('.interest').querySelectorAll('.interest__check')];
-        
-            for ( const i of arr) {    
-                i.checked = e.target.checked;
+          const arr = [...e.target.closest('.interest').querySelectorAll('.interest__check')];
+          for ( const i of arr) {    
+            i.checked = e.target.checked;
             };
 
         };
 
         if (!e.target.closest('.interest').querySelector('.interests')) {
-
-            const allChecks = [...e.target.closest('.interests').querySelectorAll('.interest__check')];
-
-            const isAllChecked = allChecks.every((check) => check.checked);
-            
+                const allChecks = [...e.target.closest('.interests').querySelectorAll('.interest__check')];
+                const isAllChecked = allChecks.every((check) => check.checked);
             if (isAllChecked) {
-                
                 e.target
-                    .closest('.interests')
-                    .closest('.interest')
-                    .querySelector('.interest__check').checked = true;
+                .closest('.interests')
+                .closest('.interest')
+                .querySelector('.interest__check').checked = true;
                   
             } else {
-                
-                e.target
+                    e.target
                     .closest('.interests')
                     .closest('.interest')
                     .querySelector('.interest__check').checked = false;
