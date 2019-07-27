f07a7db8d6743f558fdd592252e7c9cb57a2c2c0
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-07-27 12:29:58 +0300

    Update task.js

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,10 +1,10 @@
 const dropdownValue = document.querySelector('.dropdown__value');
 const dropdownList = document.querySelector('.dropdown__list');
-const dropdownItem = [...document.querySelectorAll('.dropdown__item')],
-const dropdownItemLength = dropDownItem.length;
+const dropdownItem = [...document.querySelectorAll('.dropdown__item')];
+  const dropdownItemLength = dropdownItem.length;
 
 function showDropdown() {
-    if (!dropdownList.classList.contains('dropdown__list_active')) {
+    if(!dropdownList.classList.contains('dropdown__list_active')) {
         dropdownList.classList.add('dropdown__list_active');
     } else {
         dropdownList.classList.remove('dropdown__list_active');
@@ -15,9 +15,7 @@ dropdownValue.addEventListener('click', () => {
     showDropdown();
 });
 
-
-
-for ( var i = 0; i < dropdownItemLength; i++) {
+for ( let i = 0; i < dropdownItemLength; i++) {
     dropdownItem[i].addEventListener('click', (e) => {
         e.preventDefault();
         dropdownList.classList.remove('dropdown__list_active');
