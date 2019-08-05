33230ac91993f777fbd114247b3eb144ebd61eb5
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-08-05 13:35:07 +0300

    Update task.js

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,9 @@
+const editor = document.getElementById('editor');
+
+editor.addEventListener('change', () => {
+    localStorage.setItem('editorText', editor.value);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    editor.textContent = localStorage.getItem('editorText');
+});
